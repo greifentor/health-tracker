4afae3c372945a2b3a42fe7b9ed60c68c1a0c842
ollie@medea <lieshoff@gmx.de>
2026-04-20 22:53:21 +0200

    Worked on WhoBloodPresureClassificatioService (Optimal & Normal).

--- service/src/main/java/de/ollie/healthtracker/core/service/impl/WhoBloodPressureClassificationServiceImpl.java
+++ service/src/main/java/de/ollie/healthtracker/core/service/impl/WhoBloodPressureClassificationServiceImpl.java
@@ -7,6 +7,27 @@ public class WhoBloodPressureClassificationServiceImpl implements WhoBloodPressu
 
 	@Override
 	public WhoBloodPressureClassification calculateClassification(int sysMmHg, int diaMmHg) {
-		return null;
+		return max(getSysClassification(sysMmHg), getDiaClassification(diaMmHg));
+	}
+
+	private WhoBloodPressureClassification max(
+		WhoBloodPressureClassification sysClass,
+		WhoBloodPressureClassification diaClass
+	) {
+		return sysClass.ordinal() > diaClass.ordinal() ? sysClass : diaClass;
+	}
+
+	private WhoBloodPressureClassification getSysClassification(int sysMmHg) {
+		if ((sysMmHg >= 120) && (sysMmHg <= 129)) {
+			return WhoBloodPressureClassification.NORMAL;
+		}
+		return WhoBloodPressureClassification.OPTIMAL;
+	}
+
+	private WhoBloodPressureClassification getDiaClassification(int diaMmHg) {
+		if ((diaMmHg >= 80) && (diaMmHg <= 84)) {
+			return WhoBloodPressureClassification.NORMAL;
+		}
+		return WhoBloodPressureClassification.OPTIMAL;
 	}
 }
--- service/src/test/java/de/ollie/healthtracker/core/service/impl/WhoBloodPressureClassificationServiceImplTest.java
+++ service/src/test/java/de/ollie/healthtracker/core/service/impl/WhoBloodPressureClassificationServiceImplTest.java
@@ -1,7 +1,12 @@
 package de.ollie.healthtracker.core.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import de.ollie.healthtracker.core.service.model.WhoBloodPressureClassification;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -12,5 +17,12 @@ class WhoBloodPressureClassificationServiceImplTest {
 	private WhoBloodPressureClassificationServiceImpl unitUnderTest;
 
 	@Nested
-	class calculateClassificationIntInt {}
+	class calculateClassificationIntInt {
+
+		@ParameterizedTest
+		@CsvSource({ "119,79,OPTIMAL", "120,80,NORMAL", "129,84,NORMAL", "119,84,NORMAL", "124,79,NORMAL" })
+		void returnsTheCorrectClassification(int sysMmHg, int diaMmHg, WhoBloodPressureClassification expected) {
+			assertEquals(expected, unitUnderTest.calculateClassification(sysMmHg, diaMmHg));
+		}
+	}
 }
