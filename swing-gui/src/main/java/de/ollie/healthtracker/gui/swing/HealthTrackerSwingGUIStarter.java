49b3d26b26c99ce400f1847464eef1d6fd9dd3cb
oliver.lieshoff <oliver.lieshoff@NB-PF57EQJE.Speedport_W_723V_1_49_000>
2026-07-16 17:48:37 +0200

    Changed to Spring-Boot 4.

--- swing-gui/src/main/java/de/ollie/healthtracker/gui/swing/HealthTrackerSwingGUIStarter.java
+++ swing-gui/src/main/java/de/ollie/healthtracker/gui/swing/HealthTrackerSwingGUIStarter.java
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
